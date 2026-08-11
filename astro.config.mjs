e24b8e51af77226351cdefa4c2c9a70093ea7ff0
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-08-11 19:58:19 +0800

    chore: configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static'
+  site: 'https://1264585648.github.io',
+  base: '/my_blog',
+  output: 'static',
+  trailingSlash: 'always'
 });
